043b91d9ff7a91f04512cf17d7209b71d9e7b85e
久佐賀麻友 <mayu0920@applecinnamon-Pro.local>
2025-11-08 16:46:52 +0900

    step6: style with Tailwind

--- src/views/CounterView/index.tsx
+++ src/views/CounterView/index.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export function CounterView() {
     const [count, setCount] = useState(0)
@@ -7,17 +7,27 @@ export function CounterView() {
         setCount(count + 1)
     }
     const handleMinusButtonClick = () =>{
+        console.log("マイナスボタンがおされた")
         if(count <= 0) return
         setCount(count - 1)
     }
 
+    useEffect(() => {
+        console.log("現在のカウント", count)
+    }, [count])
+
   return (
-    <div style={{ maxWidth: 400, margin: "40px auto", textAlign: "center" }}>
-      <h1>カウンター</h1>
-      <p style={{ fontSize: 32 }}>{count}</p>
+    // <div style={{ maxWidth: 400, margin: "40px auto", textAlign: "center" }}>
+    <div className="max-w-md mx-auto mt-10 text-center">
+      <h1 className="text-2xl font-bold">カウンター</h1>
+      <p className="text-4xl my-4">{count}</p>
       <div style={{ marginTop: 16, display: "flex", justifyContent: "center", gap: 8 }}>
-        <button onClick={handlePlusButtonClick}>プラス</button>
-        <button onClick={handleMinusButtonClick}>マイナス</button>
+        <button
+        className="px-4 py-2 rounded border border-gray-300 hover:bg-gray-50 active:scale-95"
+        onClick={handlePlusButtonClick}>プラス</button>
+        <button
+        className="px-4 py-2 rounded border border-gray-300 hover:bg-gray-50 active:scale-95"
+        onClick={handleMinusButtonClick}>マイナス</button>
       </div> 
     </div>
   )
