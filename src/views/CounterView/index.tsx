1c27ed18db3c155f5c109ba8d77e4bfcc989f468
久佐賀麻友 <mayu0920@applecinnamon-Pro.local>
2025-11-08 16:10:43 +0900

    step3: add minus button

--- src/views/CounterView/index.tsx
+++ src/views/CounterView/index.tsx
@@ -6,13 +6,17 @@ export function CounterView() {
     const handlePlusButtonClick = () =>{
         setCount(count + 1)
     }
+    const handleMinusButtonClick = () =>{
+        setCount(count - 1)
+    }
 
   return (
     <div style={{ maxWidth: 400, margin: "40px auto", textAlign: "center" }}>
       <h1>カウンター</h1>
       <p style={{ fontSize: 32 }}>{count}</p>
-      <div style={{ marginTop: 16 }}>
+      <div style={{ marginTop: 16, display: "flex", justifyContent: "center", gap: 8 }}>
         <button onClick={handlePlusButtonClick}>プラス</button>
+        <button onClick={handleMinusButtonClick}>マイナス</button>
       </div> 
     </div>
   )
