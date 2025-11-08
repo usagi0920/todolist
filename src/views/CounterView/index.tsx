1c5d3425f38792b474c75783af16c39cf40b3383
久佐賀麻友 <mayu0920@applecinnamon-Pro.local>
2025-11-08 16:04:11 +0900

    step2: add plus button

--- src/views/CounterView/index.tsx
+++ src/views/CounterView/index.tsx
@@ -1,9 +1,19 @@
+"use client"
+import { useState } from "react"
+
 export function CounterView() {
+    const [count, setCount] = useState(0)
+    const handlePlusButtonClick = () =>{
+        setCount(count + 1)
+    }
 
   return (
     <div style={{ maxWidth: 400, margin: "40px auto", textAlign: "center" }}>
       <h1>カウンター</h1>
-      <p style={{ fontSize: 32 }}>1</p>
+      <p style={{ fontSize: 32 }}>{count}</p>
+      <div style={{ marginTop: 16 }}>
+        <button onClick={handlePlusButtonClick}>プラス</button>
+      </div> 
     </div>
   )
 }
